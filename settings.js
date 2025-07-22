fbc569e16aaa5da1f6b64c9a41f2686b109d35a2
OwnerDEPER <syssolutions.pekerele@gmail.com>
2025-07-22 18:15:45 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;KdFwEBQZ#TQJqAElhUsRfuXPVC_NmGkLy-qXA2YbHN102f-1FRfk", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "51941847465", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
